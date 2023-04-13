50d5d426f459a527c4addc98e78028b7a779e484
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-04-13 10:52:01 -0600

    Install linters and correct linters

--- morse.rb
+++ morse.rb
@@ -1,73 +1,23 @@
-
-def   decode_morse(str)
+def decode_morse(str)
+  dict = { '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e',
+           '..-.' => 'f', '--.' => 'g', '....' => 'h', '..' => 'i',
+           '.---' => 'j', '-.-' => 'k', '.-..' => 'l', '--' => 'm',
+           '-.' => 'n', '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's',
+           '-' => 't', '..-' => 'u',
+           '...-' => 'v', '.--' => 'w', '-..-' => 'x', '-.--' => 'y',
+           '--..' => 'z' }
   words = []
-  words_morse = str.split('   ');
-  
-  words_morse.each_with_index { |word,idx| 
+  words_morse = str.split('   ')
+  words_morse.each_with_index do |word, idx|
     words.push([])
-    letters = word.split(' ')
-    letters.each {|letter|
-      if letter == '.-'
-        words[idx].push('a')
-      elsif letter == '-...'
-        words[idx].push('b')
-      elsif letter == '-.-.'
-        words[idx].push('c')
-      elsif letter == '-..'
-        words[idx].push('d')
-      elsif letter == '.'
-        words[idx].push('e')
-      elsif letter == '..-.'
-        words[idx].push('f')
-      elsif letter == '--.'
-        words[idx].push('g')
-      elsif letter == '....'
-        words[idx].push('h')  
-      elsif letter == '..'
-        words[idx].push('i')
-      elsif letter == '.---'
-        words[idx].push('j')
-      elsif letter == '-.-'
-        words[idx].push('k')
-      elsif letter == '.-..'
-        words[idx].push('l')  
-      elsif letter == '--'
-        words[idx].push('m')  
-      elsif letter == '-.'
-        words[idx].push('n')
-      elsif letter == '---'
-        words[idx].push('o')  
-      elsif letter == '.--.'
-        words[idx].push('p')  
-      elsif letter == '--.-'
-        words[idx].push('q')  
-      elsif letter == '.-.'
-        words[idx].push('r')  
-      elsif letter == '...'
-        words[idx].push('s')  
-      elsif letter == '-'
-        words[idx].push('t')  
-      elsif letter == '..-'
-        words[idx].push('u')
-      elsif letter == '...-'
-        words[idx].push('v')  
-      elsif letter == '.--'
-        words[idx].push('w')  
-      elsif letter == '-..-'
-        words[idx].push('x')  
-      elsif letter == '-.--'
-        words[idx].push('y')  
-      elsif letter == '--..'
-        words[idx].push('z')
-      end
-    }
-  }
-
-  words.each{ |e| 
-    print e.join + ' '
-  }
-  puts 
+    letters = word.split
+    letters.each do |letter|
+      words[idx].push(dict[letter])
+    end
+  end
+  words.each do |e|
+    print "#{e.join} "
+  end
+  puts
 end
-
-
-decode_morse(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+decode_morse('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
